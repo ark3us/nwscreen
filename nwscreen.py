322cddcecc5d1471a6961b02215bcf7c878cd6be
ark3us <ark3us@gmail.com>
2020-12-28 21:41:36 +0100

    Implemented GUI

--- nwscreen.py
+++ nwscreen.py
@@ -5,10 +5,68 @@ import matplotlib.widgets as widgets
 import matplotlib.backends.backend_tkagg
 import numpy as np
 import os
+import PySimpleGUI as sg
+import queue
+import threading
+import time
 from PIL import Image
+
+done = False
+
+def main():
+    images = []
+
+    layout = [
+        [sg.Input(key="_FILES_", enable_events=True, visible=False)],
+        [sg.Text("Seleziona screenshots: "), sg.FilesBrowse("...", target="_FILES_")],
+        [sg.Multiline(key="_FILELIST_", disabled=True)],
+        [sg.Button("Inizia!")],
+    ]
+
+    window = sg.Window("Screenshot Composer", layout, finalize=True, auto_size_text=True, auto_size_buttons=True, resizable=True)
+    while True:
+        event, values = window.read()
+        # print(event, values)
+        if event == sg.WIN_CLOSED:
+            break
+
+        if event == "_FILES_" and values.get("_FILES_"):
+            maxline = 0
+            images = values.get("_FILES_").split(";")
+            for f in images:
+                if len(f) > maxline:
+                    maxline = len(f)
+
+            maxcol = len(images)
+            if maxline > 200:
+                maxline = 200
+            if maxcol > 20:
+                maxcol = 20
+
+            window["_FILELIST_"].Update("\n".join(images))
+            window["_FILELIST_"].set_size((maxline, maxcol))
+        
+        elif event == "Inizia!":
+            progress = queue.Queue()
+            th = threading.Thread(target=process, args=(images, progress))
+            th.daemon = True
+            th.start()
+
+            while progress.qsize() < len(images) or done:
+                time.sleep(0.5)
+                if progress.qsize() > 0:
+                    sg.OneLineProgressMeter('Processando le immagini...', progress.qsize(), len(images), 'key')
 
 
 x1 = x2 = y1 = y2 = 0
+
+
+def process(images, progress):
+    try:
+        _process(images, progress)
+    finally:
+        global done
+        done = True
 
 
 def onselect(eclick, erelease):
@@ -20,25 +78,10 @@ def onselect(eclick, erelease):
     plt.close()
 
 
-def main():
-    if not os.path.exists("pics"):
-        os.mkdir("pics")
-
-    images = []
-
-    for root, _, files in os.walk("pics"):
-        for f in files:
-            if "screenshots_here" in f:
-                continue
-            filename = os.path.join(root, f)
-            images.append(filename)
-    
+def _process(images: list, progress: queue.Queue):
     if not images:
-        input("Metti gli screenshot nella cartella 'pics' e avvia di nuovo il programma...")
         return
 
-    # Sort by creation time
-    images.sort(key=os.path.getctime)
     newimg = Image.new("RGB", (1,1))
     rows = 0
     cols = 0
@@ -55,37 +98,42 @@ def main():
             ax = fig.add_subplot(111)
             arr = np.asarray(im)
             plt.imshow(arr)
-            rs=widgets.RectangleSelector(
+            rs = widgets.RectangleSelector(
                 ax, onselect, drawtype='box',
                 rectprops = dict(facecolor='red', edgecolor = 'black', alpha=0.5, fill=True))
-            
+
             mng = plt.get_current_fig_manager()
-            mng.resize(*mng.window.maxsize())
+            if os.name == 'nt':
+                mng.window.state('zoomed')
+            else:
+                mng.resize(*mng.window.maxsize())
             plt.show()
         
         if rows == 5:
             cols += 1
             rows = 0
 
-        im1 = im.crop((x1, y1, x2, y2))
+        im1 = im.crop((x1, y1, x2, y2)).convert("RGB")
+        # Delete tells
+        image_data = im1.load()
+        height, width = im1.size
+        for loop1 in range(height):
+            for loop2 in range(width):
+                r, g, b = image_data[loop1,loop2]
+                if r < 50 and b < 50 and g > 170:
+                    for x in range(5, loop1+7):
+                        for y in range(loop2-7, loop2+7):
+                            image_data[x, y] = r, 169, b
+
         tmpimg = Image.new("RGB", (maxrows*(x1+x2), (cols+1)*(y2-y1)))
         tmpimg.paste(newimg, (0, 0))
         tmpimg.paste(im1, (rows*(x1+x2), cols*(y2-y1)))
         newimg = tmpimg
         rows += 1
+    
+        progress.put(filename)
 
     # newimg.show()
-
-    # Delete tells
-    image_data = newimg.load()
-    height, width = newimg.size
-    for loop1 in range(height):
-        for loop2 in range(width):
-            r,g,b = image_data[loop1,loop2]
-            if r < 50 and b < 50 and g > 170:
-                for x in range(loop1-5, loop1+5):
-                    for y in range(loop2-7, loop2+7):
-                        image_data[x, y] = r, 169, b
 
     newimg.show()
 
